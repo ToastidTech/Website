6f0c72e3fbe547659746c26f490275dbe1a3edff
ToastidTech <toastidtechllc@gmail.com>
2026-07-06 16:37:13 -0500

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,74 @@
+const CACHE = 'toastid-v1';
+
+const ASSETS = [
+
+'/',
+'/index.html',
+'/products.html',
+'/about.html',
+'/contact.html',
+'/disclaimer.html',
+
+'/manifest.json',
+
+'/assets/styles.css',
+'/assets/site.js',
+
+'/assets/logos/shield-logo.jpg',
+'/assets/logos/icon-192.png',
+'/assets/logos/icon-512.png'
+
+];
+
+self.addEventListener('install', e => {
+
+e.waitUntil(
+
+caches.open(CACHE)
+.then(cache => cache.addAll(ASSETS))
+
+);
+
+self.skipWaiting();
+
+});
+
+self.addEventListener('activate', e => {
+
+e.waitUntil(
+
+caches.keys().then(keys =>
+
+Promise.all(
+
+keys.map(key => {
+
+if(key !== CACHE){
+
+return caches.delete(key);
+
+}
+
+})
+
+)
+
+)
+
+);
+
+clients.claim();
+
+});
+
+self.addEventListener('fetch', e => {
+
+e.respondWith(
+
+fetch(e.request)
+
+.catch(() => caches.match(e.request))
+
+);
+
+});
