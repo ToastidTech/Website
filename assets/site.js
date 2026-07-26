49f4f340df8c13988752831d6e7a2c60670d599b
ToastidTech <toastidtechllc@gmail.com>
2026-07-26 01:44:42 -0500

    Update site.js

--- assets/site.js
+++ assets/site.js
@@ -181,7 +181,7 @@
 /* ---------- Service worker registration ---------- */
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
-    navigator.serviceWorker.register('sw.js').catch(function (err) {
+    navigator.serviceWorker.register('/sw.js').catch(function (err) {
       console.log('SW registration failed:', err);
     });
   });
